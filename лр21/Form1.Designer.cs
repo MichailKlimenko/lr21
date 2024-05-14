2afac3538b0f10acd276d1ed4c922330b5b6b6b6
WolfGGO <klimenkomisa1@gmail.com>
2024-05-14 21:49:11 +0300

    Добавьте файлы проекта.

---
+++ лр21/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace лр21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.задание3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.задание3ToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(524, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // задание3ToolStripMenuItem
+            // 
+            this.задание3ToolStripMenuItem.Name = "задание3ToolStripMenuItem";
+            this.задание3ToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.задание3ToolStripMenuItem.Text = "Задание 3";
+            this.задание3ToolStripMenuItem.Click += new System.EventHandler(this.задание3ToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(25, 20);
+            this.toolStripMenuItem1.Text = "4";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(25, 20);
+            this.toolStripMenuItem2.Text = "5";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 363);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Задание7";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem задание3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+    }
+}
+
---
+++ лр21/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр21
+{
+    public partial class Form1 : Form
+    {
+        // Объявление таймера для обновления времени на аналоговых часах
+        private Timer clockTimer = new Timer();
+        // Флаг, определяющий, работают ли часы в данный момент
+        private bool isClockRunning = false;
+        // Радиус циферблата
+        private const int clockRadius = 80;
+        // Длина часовой стрелки
+        private const int hourHandLength = 50;
+        // Длина минутной стрелки
+        private const int minuteHandLength = 70;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Установка интервала таймера для обновления времени на аналоговых часах
+            clockTimer.Interval = 1000;
+            clockTimer.Tick += ClockTimer_Tick;
+        }
+
+        // Обработчик события перерисовки формы
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Рисование графика функции
+            DrawGraph(e.Graphics);
+            // Рисование аналоговых часов
+            DrawAnalogClock(e.Graphics);
+        }
+
+        // Рисование графика функции
+        private void DrawGraph(Graphics g)
+        {
+            Pen graphPen = new Pen(Color.Blue, 2);
+            for (int x = -ClientSize.Width / 2; x <= ClientSize.Width / 2; x++)
+            {
+                float y = -2 * x * x + 3 * x * 3;
+                g.FillRectangle(Brushes.Red, ClientSize.Width / 2 + x, ClientSize.Height / 2 - y, 2, 2);
+            }
+        }
+
+        // Рисование аналоговых часов
+        private void DrawAnalogClock(Graphics g)
+        {
+            // Определение текущего времени
+            DateTime currentTime = DateTime.Now;
+            int centerX = ClientSize.Width / 2;
+            int centerY = ClientSize.Height / 2;
+
+            // Рисование круга для циферблата
+            g.DrawEllipse(Pens.Black, centerX - clockRadius, centerY - clockRadius, 2 * clockRadius, 2 * clockRadius);
+
+            // Рисование цифр на циферблате
+            for (int i = 1; i <= 12; i++)
+            {
+                double angle = 2 * Math.PI * i / 12 - Math.PI / 2;
+                int x = (int)(centerX + (clockRadius - 15) * Math.Cos(angle));
+                int y = (int)(centerY + (clockRadius - 15) * Math.Sin(angle));
+                g.DrawString(i.ToString(), Font, Brushes.Black, x, y);
+            }
+
+            // Рисование часовой стрелки
+            double hourAngle = 2 * Math.PI * (currentTime.Hour % 12 + currentTime.Minute / 60.0) / 12 - Math.PI / 2;
+            int hourX = (int)(centerX + hourHandLength * Math.Cos(hourAngle));
+            int hourY = (int)(centerY + hourHandLength * Math.Sin(hourAngle));
+            g.DrawLine(Pens.Black, centerX, centerY, hourX, hourY);
+
+            // Рисование минутной стрелки
+            double minuteAngle = 2 * Math.PI * (currentTime.Minute + currentTime.Second / 60.0) / 60 - Math.PI / 2;
+            int minuteX = (int)(centerX + minuteHandLength * Math.Cos(minuteAngle));
+            int minuteY = (int)(centerY + minuteHandLength * Math.Sin(minuteAngle));
+            g.DrawLine(Pens.Black, centerX, centerY, minuteX, minuteY);
+
+            // Рисование секундной стрелки
+            double secondAngle = 2 * Math.PI * currentTime.Second / 60 - Math.PI / 2;
+            int secondX = (int)(centerX + minuteHandLength * Math.Cos(secondAngle));
+            int secondY = (int)(centerY + minuteHandLength * Math.Sin(secondAngle));
+            g.DrawLine(Pens.Red, centerX, centerY, secondX, secondY);
+        }
+
+        // Обработчик события срабатывания таймера
+        private void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            // Обновление времени на аналоговых часах
+            Invalidate();
+        }
+
+        // Обработчик нажатия кнопки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Запуск или остановка таймера для обновления времени на аналоговых часах
+            if (isClockRunning)
+            {
+                clockTimer.Stop();
+                isClockRunning = false;
+                button1.Text = "Start";
+            }
+            else
+            {
+                clockTimer.Start();
+                isClockRunning = true;
+                button1.Text = "Stop";
+            }
+        }
+    }
+}
